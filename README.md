1a4d2580047e525983b5036245a4d96710fb9a3f
Tech-maniac <57858304+Tech-maniac@users.noreply.github.com>
2021-05-04 15:39:26 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# mohiuddin_capstone
+# AngryBirdsStage3
+Stage 3 Angry Birds: Introducing Constraint
---
+++ sketch.js
@@ -0,0 +1,75 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+
+var engine, world;
+var box1, pig1,pig3;
+var backgroundImg,platform;
+var bird, slingshot;
+
+var gameState = "onSling";
+
+
+
+
+function preload()
+{
+    bg= loadImage("sprites/bg.png")
+}
+function setup(){
+    var canvas = createCanvas(1200,400);
+    engine = Engine.create();
+    world = engine.world;
+
+
+    ground = new Ground(600,height,1200,20);
+ 
+
+    box1 = new Box(700,320,70,70);
+    box2 = new Box(920,320,70,70);
+    pig1 = new Pig(810, 350);
+  
+
+    box3 = new Box(700,240,70,70);
+    box4 = new Box(920,240,70,70);
+    pig3 = new Pig(810, 220);
+
+    
+
+    box5 = new Box(810,160,70,70);
+   
+
+
+
+  
+   
+}
+
+function draw(){
+
+        background(bg);
+
+    Engine.update(engine);
+    //strokeWeight(4);
+    box1.display();
+    box2.display();
+    ground.display();
+    pig1.display();
+    pig1.score();
+    
+
+    box3.display();
+    box4.display();
+    pig3.display();
+    pig3.score();
+
+
+    box5.display();
+  
+
+
+
+  
+}
+
